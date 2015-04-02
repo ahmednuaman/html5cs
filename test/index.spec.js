52f071f9b7c47c1843583c9bcc95bac74690ac89
Ahmed Nuaman <ahmed@ahmednuaman.com>
2015-04-02 20:27:17 +0100

    getting tests going

---
+++ test/index.spec.js
@@ -0,0 +1,12 @@
+var expect = require('expect.js'),
+    html5cs = require('../index');
+
+describe('html5cs', function () {
+  it('should be happy with good.html', function (done) {
+    done();
+  });
+
+  it('should complain about bad.html', function (done) {
+    done();
+  });
+});
